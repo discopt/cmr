cc5cb221e10d43fd3e9e109a808f987d17920822
Matthias Walter <xammy@xammy.homelinux.net>
2010-05-10 21:56:11 +0200

    Updated style and documentation of permutation handling.

--- src/permutations.hpp
+++ src/permutations.hpp
@@ -18,6 +18,10 @@
 
 namespace tu {
 
+  /**
+   * Exception to indicate that the size of a permutation could not be be reduced.
+   */
+
   class permutation_shrink_exception: std::exception
   {
   public:
@@ -37,6 +41,12 @@ namespace tu {
     }
   };
 
+  /**
+   * A permutation which maps integers from 0 to size-1 to the same range
+   * in any possible way. This implementation stores the image of each
+   * of the values in a vector.
+   */
+
   class permutation
   {
   public:
@@ -46,62 +56,120 @@ namespace tu {
     typedef std::vector <value_type> data_type;
 
   protected:
-    data_type data_;
+    data_type _data;
 
   public:
+
+    /**
+     * Constructs a permutation of a given size,
+     * initializing to the identity.
+     *
+     * @param size Optional size of the permutation
+     */
+
     permutation (size_type size = 0)
     {
       reset(size);
     }
 
+    /**
+     * Copy constructor
+     *
+     * @param other Another permutation
+     */
+
     permutation (const permutation& other)
     {
-      data_.resize(other.size());
-      for (size_type i = 0; i < data_.size(); ++i)
-        data_[i] = other.data_[i];
+      _data.resize(other.size());
+      for (size_type i = 0; i < _data.size(); ++i)
+        _data[i] = other._data[i];
     }
+
+    /**
+     * Destructor
+     */
 
     virtual ~permutation ()
     {
 
     }
 
+    /**
+     * Resizes the permutation and resets it to identity.
+     *
+     * @param new_size New size
+     */
+
     void reset (size_t new_size)
     {
-      data_.resize(new_size);
+      _data.resize(new_size);
       for (size_type i = 0; i < new_size; ++i)
       {
-        data_[i] = i;
+        _data[i] = i;
       }
     }
 
+    /**
+     * Resets the permutation to identity.
+     */
+
     inline void reset ()
     {
-      reset(data_.size());
+      reset(_data.size());
     }
+
+    /**
+     * @return The current size of the permutation
+     */
 
     inline size_type size () const
     {
-      return data_.size();
+      return _data.size();
     }
 
-    // Operations
+    /**
+     * The image of a specific integer in range 0 .. size-1.
+     *
+     * @param index Given integer
+     * @return The integers image
+     */
 
     inline value_type operator() (value_type index) const
     {
-      assert (index < data_.size());
       return get(index);
     }
 
+    /**
+     * The image of a specific integer in range 0 .. size-1.
+     * @param index Given integer
+     * @return The integers image
+     */
+
     inline value_type get (value_type index) const
     {
-      return data_[index];
+      assert (index < _data.size());
+
+      return _data[index];
     }
+
+    /**
+     * Swaps the images of two integers in range 0 .. size-1.
+     *
+     * @param a First integer
+     * @param b Second integer
+     */
 
     inline void swap (value_type a, value_type b)
     {
-      std::swap(data_[a], data_[b]);
+      std::swap(_data[a], _data[b]);
     }
+
+    /**
+     * Swaps the preimages of two integers in range 0 .. size-1.
+     *
+     * @param a First integer
+     * @param b Second integer
+     */
 
     void rswap (value_type a, value_type b)
     {
@@ -113,33 +181,57 @@ namespace tu {
       swap(pa, pb);
     }
 
+    /**
+     * Makes this permutation its own inverse.
+     */
+
     void revert ()
     {
-      // temporary copy
+      /// Create a temporary copy
       value_type* temp = new value_type[size()];
       for (size_type i = 0; i < size(); ++i)
-        temp[i] = data_[i];
+        temp[i] = _data[i];
 
       for (size_type i = 0; i < size(); ++i)
-        data_[temp[i]] = i;
+        _data[temp[i]] = i;
       delete[] temp;
     }
+
+    /**
+     * @return The inverse permutation of this permutation
+     */
 
     permutation reverse () const
     {
       permutation result(size());
       for (size_type i = 0; i < size(); ++i)
-        result.data_[get(i)] = i;
+        result._data[get(i)] = i;
       return result;
     }
 
+    /**
+     * Assignment operator.
+     *
+     * @param other Another permutation
+     * @return A reference to this permutation
+     */
+
     permutation& operator= (const permutation& other)
     {
-      data_.resize(other.size());
-      for (size_type i = 0; i < data_.size(); ++i)
-        data_[i] = other.data_[i];
+      _data.resize(other.size());
+      for (size_type i = 0; i < _data.size(); ++i)
+        _data[i] = other._data[i];
       return *this;
     }
+
+    /**
+     * Calculates the product of this and another permutation.
+     * The result is equivalent to applying the second permutation
+     * first and this one afterwards.
+     *
+     * @param rhs Right hand side permutation
+     * @return The resulting permutation
+     */
 
     permutation operator* (const permutation& rhs) const
     {
@@ -147,31 +239,49 @@ namespace tu {
 
       permutation result(size());
       for (size_type i = 0; i < size(); ++i)
-        result._set(i, data_[rhs(i)]);
+        result._set(i, _data[rhs(i)]);
 
       return result;
     }
 
-    // Resizing stuff
+    /**
+     * Resizes the permutation, retaining the contents.
+     * This may fail if it cannot be shrunken this way and
+     * will throw a permutation_shrink_exception in that case.
+     *
+     * @param new_size New size of the permutation
+     */
 
     void resize (size_type new_size)
     {
       size_type old_size = size();
       for (size_type i = new_size; i < old_size; ++i)
       {
-        if (data_[i] < new_size)
+        if (_data[i] < new_size)
           throw permutation_shrink_exception();
       }
 
-      data_.resize(new_size);
+      _data.resize(new_size);
       for (size_type i = old_size; i < new_size; i++)
-        data_[i] = i;
+        _data[i] = i;
     }
+
+    /**
+     * Grows the permutation by a given number of elements.
+     *
+     * @param by Number of elements to increase the size by
+     */
 
     inline void grow (difference_type by)
     {
       resize(size() + by);
     }
+
+    /**
+     * Shrinks the permutation by a given number of elements.
+     *
+     * @param by Number of elements to decrease the size by
+     */
 
     inline void shrink (difference_type by)
     {
@@ -180,28 +290,48 @@ namespace tu {
 
   protected:
 
+    /// Class to enumerate permutations
+
     friend class permutation_enumerator;
+
+    /// Class to setup a permutation which can be put before a vector.
 
     template <class Less>
     friend void sort (permutation& permutation, size_t first, size_t beyond, Less& less);
 
+    /**
+     * Sets a specific value without checking validity.
+     *
+     * @param index Index of the entry
+     * @param value New value
+     */
+
     void _set (value_type index, value_type value)
     {
-      data_[index] = value;
+      _data[index] = value;
     }
+
+    /**
+     * @return A reference to the data vector
+     */
 
     inline data_type& get_data ()
     {
-      return data_;
+      return _data;
     }
 
   };
+
+  /**
+   * Enumerates all permutations of a given size with
+   * optional constraints. You must derive from it
+   * and override the visitor() method.
+   */
 
   class permutation_enumerator
   {
   public:
     typedef size_t size_type;
-    typedef std::vector <permutation::value_type> groups_type;
     typedef std::vector <permutation::value_type> memberlist_type;
     typedef std::pair <memberlist_type*, permutation*> groupinfo_type;
     typedef std::map <int, groupinfo_type> state_type;
@@ -212,6 +342,14 @@ namespace tu {
     permutation _permutation;
 
   public:
+
+    /**
+     * Constructs an enumerator without constraints.
+     * Has O(size!) running time.
+     *
+     * @param size Size of the permutations
+     */
+
     permutation_enumerator (permutation::size_type size) :
       _permutation(size)
     {
@@ -226,7 +364,15 @@ namespace tu {
       }
     }
 
-    permutation_enumerator (const groups_type& groups) :
+    /**
+     * Constructs an enumerator with grouping constraints:
+     * Only those permutations are enumerated where k and p(k) are
+     * in the same group for all i.
+     *
+     * @param groups Vector of groups
+     */
+
+    permutation_enumerator (const std::vector <permutation::value_type>& groups) :
       _permutation(groups.size())
     {
       _size = groups.size();
@@ -250,6 +396,10 @@ namespace tu {
       }
     }
 
+    /**
+     * Destructor.
+     */
+
     virtual ~permutation_enumerator ()
     {
       for (state_type::iterator iter = _state.begin(); iter != _state.end(); ++iter)
@@ -259,10 +409,21 @@ namespace tu {
       }
     }
 
+    /**
+     * @return true if and only if the size of each permutation is zero
+     */
+
     inline bool empty ()
     {
       return _size == 0;
     }
+
+    /**
+     * Starts the enumeration which is aborted if any
+     * visitor returns false.
+     *
+     * @return true if and only if all visitors returned true
+     */
 
     virtual bool enumerate ()
     {
@@ -274,12 +435,29 @@ namespace tu {
 
   protected:
 
+    /**
+     * Method which is called for each enumerated permutation.
+     * Needs to be overridden.
+     *
+     * @param perm Current permutation
+     * @return false to abort enumeration
+     */
+
     virtual bool visitor (const permutation& perm) = 0;
 
   private:
 
+    /**
+     * Enumerates possible values at a given index in a given group.
+     *
+     * @param state Current state and group
+     * @param index Current index to enumerate values
+     * @return false to abort enumeration
+     */
+
     bool enumerate_group (state_type::iterator state, permutation::size_type index)
     {
+      /// Nothing to enumerate - just call the visitor.
       if (state == _state.end())
       {
         for (state = _state.begin(); state != _state.end(); ++state)
@@ -295,13 +473,14 @@ namespace tu {
         return visitor(_permutation);
       }
 
+      /// Jump to next group
       if (index >= state->second.first->size())
       {
         return enumerate_group(++state, 0);
       }
 
+      /// Enumerate the current groups permutation.
       permutation& p = *(state->second.second);
-
       for (permutation::size_type i = 0; i < p.size(); i++)
       {
         bool found = false;
@@ -325,6 +504,14 @@ namespace tu {
     }
   };
 
+  /**
+   * Compares two permutations for equality.
+   *
+   * @param p First permutation
+   * @param q Second permutation
+   * @return true if and only if the permutations are the same
+   */
+
   inline bool operator== (const permutation& p, const permutation& q)
   {
     if (p.size() != q.size())
@@ -338,6 +525,14 @@ namespace tu {
     return true;
   }
 
+  /**
+   * Output operator for permutations.
+   *
+   * @param stream A given output stream
+   * @param p A given permutation
+   * @return The stream after writing the permutation
+   */
+
   inline std::ostream& operator<< (std::ostream& stream, const permutation& p)
   {
     if (p.size() > 0)
@@ -349,12 +544,30 @@ namespace tu {
     return stream;
   }
 
+  /**
+   * Arranges part of a given permutation such that it can be used to
+   * view a vector in a sorted way.
+   *
+   * @param permutation The given permutation to be changed
+   * @param first First index of the part
+   * @param beyond Beyond index of the part
+   * @param less Functor to compare two elements
+   */
+
   template <class Less>
   inline void sort (permutation& permutation, size_t first, size_t beyond, Less& less)
   {
     permutation::data_type& data = permutation.get_data();
     std::sort(data.begin() + first, data.begin() + beyond, less);
   }
+
+  /**
+   * Arranges a given permutation such that it can be used to
+   * view a vector in a sorted way.
+   *
+   * @param permutation The given permutation to be changed
+   * @param less Functor to compare two elements
+   */
 
   template <class Less>
   inline void sort (permutation& permutation, Less& less)
