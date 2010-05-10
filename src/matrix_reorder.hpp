7c3169453b1fd07f7162589e3e0cf072e5121689
Matthias Walter <xammy@xammy.homelinux.net>
2010-05-10 23:00:24 +0200

    Updated style and documentation for matrix reordering routines.

--- src/matrix_reorder.hpp
+++ src/matrix_reorder.hpp
@@ -8,11 +8,18 @@
 #ifndef MATRIX_REORDER_HPP_
 #define MATRIX_REORDER_HPP_
 
-#include "../config.h"
 #include "permutations.hpp"
 #include "matroid_transposed.hpp"
 
 namespace tu {
+
+  /**
+   * Applies a row permutation to a given matrix, i.e. performs the
+   * necessary row swaps at the matrix directly.
+   *
+   * @param matrix The matrix to be changed.
+   * @param perm The given permutation
+   */
 
   template <typename MatrixType>
   inline void matrix_apply_row_permutation (MatrixType& matrix, const permutation& perm)
@@ -26,6 +33,14 @@ namespace tu {
     }
   }
 
+  /**
+   * Applies a columnn permutation to a given matrix, i.e. performs the
+   * necessary column swaps at the matrix directly.
+   *
+   * @param matrix The matrix to be changed.
+   * @param perm The given permutation
+   */
+
   template <typename MatrixType>
   inline void matrix_apply_column_permutation (MatrixType& matrix, const permutation& perm)
   {
@@ -33,36 +48,49 @@ namespace tu {
     matrix_apply_row_permutation(transposed, perm);
   }
 
+  /**
+   * A functor to compare rows of a matrix lexicographically.
+   */
+
   template <typename MatrixType, typename Less>
   struct matrix_reorder_row_less
   {
+    /**
+     * Creates the functor.
+     *
+     * @param matrix The given matrix
+     * @param column_first First column to compare at
+     * @param column_beyond Beyond column to compare at
+     * @param less Used functor to compare elements
+     */
+
     matrix_reorder_row_less (const MatrixType& matrix, size_t column_first, size_t column_beyond, Less less) :
       matrix_(matrix), column_first_(column_first), column_beyond_(column_beyond), less_(less)
     {
 
     }
 
+    /**
+     * Applies the functor.
+     *
+     * @param a First row index
+     * @param b Second row index
+     * @return false if and only if the first row is lexicographically greater than the second.
+     */
+
     bool operator() (size_t a, size_t b)
     {
-      //        std::cout << "comparing row " << a << " with " << b << std::endl;
       for (size_t column = column_first_; column < column_beyond_; ++column)
       {
         typename MatrixType::value_type va = matrix_(a, column);
         typename MatrixType::value_type vb = matrix_(b, column);
 
         if (less_(va, vb))
-        {
-          //                std::cout << "column " << column << " said a < b" << std::endl;
           return true;
-        }
         else if (less_(vb, va))
-        {
-          //                std::cout << "column " << column << " said a > b" << std::endl;
           return false;
-        }
       }
 
-      //        std::cout << "a = b" << std::endl;
       return false;
     }
 
@@ -72,20 +100,38 @@ namespace tu {
     Less less_;
   };
 
+  /**
+   * Reorders the rows of a matrix by setting up a permutation to view the ordering.
+   *
+   * @param matrix The given matrix, which is unchanged.
+   * @param row_first First row to reorder
+   * @param row_beyond Beyond row to reorder
+   * @param column_first First column for comparison
+   * @param column_beyond Beyond column for comparison
+   * @param element_less Compare functor
+   * @param result_permutation Resulting permutation
+   */
+
   template <typename MatrixType, typename ElementLess>
   inline void matrix_reorder_rows (const MatrixType& matrix, size_t row_first, size_t row_beyond, size_t column_first, size_t column_beyond,
       ElementLess element_less, permutation& result_permutation)
   {
-    //    std::cout << "matrix_reorder_rows (with permutation)" << std::endl;
     result_permutation.reset(matrix.size1());
-    //    std::cout << "constructing comparator" << std::endl;
     matrix_reorder_row_less <MatrixType, ElementLess> less(matrix, column_first, column_beyond, element_less);
-    //    result_permutation.write (std::cout);
-    //    std::cout << std::endl;
-    //    std::cout << "sorting in [" << row_first << ", " << row_beyond << ")" << std::endl;
     sort(result_permutation, row_first, row_beyond, less);
-    //    std::cout << "sorted" << std::endl;
   }
+
+  /**
+   * Reorders the columns of a matrix by setting up a permutation to view the ordering.
+   *
+   * @param matrix The given matrix, which is unchanged.
+   * @param row_first First row for comparison
+   * @param row_beyond Beyond row for comparison
+   * @param column_first First column to reorder
+   * @param column_beyond Beyond column to reorder
+   * @param element_less Compare functor
+   * @param result_permutation Resulting permutation
+   */
 
   template <typename MatrixType, typename ElementLess>
   inline void matrix_reorder_columns (const MatrixType& matrix, size_t row_first, size_t row_beyond, size_t column_first, size_t column_beyond,
@@ -95,20 +141,36 @@ namespace tu {
     matrix_reorder_rows(transposed, column_first, column_beyond, row_first, row_beyond, element_less, result_permutation);
   }
 
+  /**
+   * Reorders the rows of a matrix.
+   *
+   * @param matrix The given matrix, which is unchanged.
+   * @param row_first First row for comparison
+   * @param row_beyond Beyond row for comparison
+   * @param column_first First column to reorder
+   * @param column_beyond Beyond column to reorder
+   * @param element_less Compare functor
+   */
+
   template <typename MatrixType, typename ElementLess>
   inline void matrix_reorder_rows (MatrixType& matrix, size_t row_first, size_t row_beyond, size_t column_first, size_t column_beyond,
       ElementLess element_less)
   {
     permutation perm;
-    //    std::cout << "matrix_reorder_rows" << std::endl;
     matrix_reorder_rows(matrix, row_first, row_beyond, column_first, column_beyond, element_less, perm);
-    //    std::cout << "applying permutation: " << std::endl;
-    //    perm.write(std::cout);
-    //    std::cout << std::endl;
-    //    perm.revert();
     matrix_apply_row_permutation(matrix, perm);
-    //    std::cout << "applied" << std::endl;
   }
+
+  /**
+   * Reorders the columns of a matrix.
+   *
+   * @param matrix The given matrix, which is unchanged.
+   * @param row_first First row for comparison
+   * @param row_beyond Beyond row for comparison
+   * @param column_first First column to reorder
+   * @param column_beyond Beyond column to reorder
+   * @param element_less Compare functor
+   */
 
   template <typename MatrixType, typename ElementLess>
   inline void matrix_reorder_columns (MatrixType& matrix, size_t row_first, size_t row_beyond, size_t column_first, size_t column_beyond,
