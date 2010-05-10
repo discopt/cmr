d1371a4b61efebb2e67b29c7d67778476b8bacb4
Matthias Walter <xammy@xammy.homelinux.net>
2010-05-10 23:50:44 +0200

    Updated style and documentation for matroid reordering routines.

--- src/matroid_reorder.hpp
+++ src/matroid_reorder.hpp
@@ -8,10 +8,18 @@
 #ifndef MATROID_REORDER_HPP_
 #define MATROID_REORDER_HPP_
 
-#include "../config.h"
 #include "matrix_reorder.hpp"
 
 namespace tu {
+
+  /**
+   * Applies a row permutation to a given matroid and its representation matrix, i.e. performs the
+   * necessary row swaps at the matroid / matrix directly.
+   *
+   * @param matroid The given matroid to be changed
+   * @param matrix Representation matrix of the given matroid
+   * @param perm The given permutation
+   */
 
   template <typename MatroidType, typename MatrixType>
   inline void matroid_apply_row_permutation (MatroidType& matroid, MatrixType& matrix, const permutation& perm)
@@ -26,6 +34,15 @@ namespace tu {
     }
   }
 
+  /**
+   * Applies a column permutation to a given matroid and its representation matrix, i.e. performs the
+   * necessary columns swaps at the matroid / matrix directly.
+   *
+   * @param matroid The given matroid to be changed
+   * @param matrix Representation matrix of the given matroid
+   * @param perm The given permutation
+   */
+
   template <typename MatroidType, typename MatrixType>
   inline void matroid_apply_column_permutation (MatroidType& matroid, MatrixType& matrix, const permutation& perm)
   {
@@ -34,17 +51,38 @@ namespace tu {
     matroid_apply_row_permutation(transposed_matroid, transposed_matrix, perm);
   }
 
+  /**
+   * Reorders the rows of a matroid and its representation matrix by setting up a permutation to view the ordering.
+   *
+   * @param matroid The given matrix, which is unchanged.
+   * @param matrix Representation matrix of the given matroid
+   * @param row_first First row to reorder
+   * @param row_beyond Beyond row to reorder
+   * @param column_first First column for comparison
+   * @param column_beyond Beyond column for comparison
+   * @param element_less Compare functor
+   */
+
   template <typename MatroidType, typename MatrixType, typename ElementLess>
   inline void matroid_reorder_rows (MatroidType& matroid, MatrixType& matrix, size_t row_first, size_t row_beyond, size_t column_first,
       size_t column_beyond, ElementLess element_less)
   {
     permutation perm;
     matrix_reorder_rows(matrix, row_first, row_beyond, column_first, column_beyond, element_less, perm);
-    //    std::cout << "applying permutation of: " << std::endl;
-    //    perm.write (std::cout);
-    //    std::cout << std::endl;
     matroid_apply_row_permutation(matroid, matrix, perm);
   }
+
+  /**
+   * Reorders the columns of a matroid and its representation matrix by setting up a permutation to view the ordering.
+   *
+   * @param matroid The given matrix, which is unchanged.
+   * @param matrix Representation matrix of the given matroid
+   * @param row_first First row for comparison
+   * @param row_beyond Beyond row for comparison
+   * @param column_first First column to reorder
+   * @param column_beyond Beyond column to reorder
+   * @param element_less Compare functor
+   */
 
   template <typename MatroidType, typename MatrixType, typename ElementLess>
   inline void matroid_reorder_columns (MatroidType& matroid, MatrixType& matrix, size_t row_first, size_t row_beyond, size_t column_first,
