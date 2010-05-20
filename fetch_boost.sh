3e2d7ef86d567a224c3823cd342baa5b5c0668e8
Matthias Walter <xammy@xammy.homelinux.net>
2010-05-21 00:26:39 +0200

    Added some verbosity and some dependencies to boost-fetch script.

--- fetch_boost.sh
+++ fetch_boost.sh
@@ -4,14 +4,14 @@ BOOST_URL=http://downloads.sourceforge.net/project/boost/boost/1.43.0/boost_1_43
 BOOST_TGZ=boost_1_43_0.tar.bz2
 
 BOOST_FOLDERS=
-for FOLDER in numeric/ublas serialization config mpl type_traits iterator tuple integer multi_index property_map concept smart_ptr optional archive dynamic_bitset parameter range bind python; do
+for FOLDER in numeric/ublas serialization config mpl type_traits iterator tuple integer multi_index property_map concept smart_ptr optional archive dynamic_bitset parameter range bind python graph detail preprocessor utility exception unordered pending functional; do
   BOOST_FOLDERS="${BOOST_FOLDERS} boost_1_43_0/boost/${FOLDER}"
 done
 
 cd src
 rm -rf boost
 wget ${BOOST_URL} -O ${BOOST_TGZ}
-tar xjf ${BOOST_TGZ} --wildcards --no-wildcards-match-slash 'boost_1_43_0/boost/*.hpp' ${BOOST_FOLDERS}
+tar xjvf ${BOOST_TGZ} --wildcards --no-wildcards-match-slash 'boost_1_43_0/boost/*.hpp' ${BOOST_FOLDERS}
 mv boost_1_43_0/boost boost
 rm -rf boost_1_43_0
 rm ${BOOST_TGZ}
