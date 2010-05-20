48651249061b44cc0b72ec99b56fcf2d3e74ad38
Matthias Walter <xammy@xammy.homelinux.net>
2010-05-20 23:29:58 +0200

    Added a script which fetches boost and extracts necessary parts.

---
+++ fetch_boost.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+#BOOST_URL=http://downloads.sourceforge.net/project/boost/boost/1.43.0/boost_1_43_0.tar.bz2
+BOOST_URL=/home/xammy/boost_1_43_0.tar.bz2
+BOOST_TGZ=boost_1_43_0.tar.bz2
+
+BOOST_FOLDERS=
+for FOLDER in numeric/ublas serialization config mpl type_traits iterator tuple integer multi_index property_map concept smart_ptr optional archive dynamic_bitset parameter range bind python; do
+  BOOST_FOLDERS="${BOOST_FOLDERS} boost_1_43_0/boost/${FOLDER}"
+done
+
+cd src
+rm -rf boost
+#wget ${BOOST_URL} -O ${BOOST_TGZ}
+cp ${BOOST_URL}  ${BOOST_TGZ}
+tar xjf ${BOOST_TGZ} --wildcards --no-wildcards-match-slash 'boost_1_43_0/boost/*.hpp' ${BOOST_FOLDERS}
+mv boost_1_43_0/boost boost
+rm -rf boost_1_43_0
+rm ${BOOST_TGZ}
