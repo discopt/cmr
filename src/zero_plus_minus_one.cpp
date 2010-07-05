610104627f34f1ee34b67f48d703575c4b87f0cb
Matthias Walter <xammy@xammy.homelinux.net>
2010-07-05 15:36:04 +0200

    Changed type of 1x1 violator matrices.

--- src/zero_plus_minus_one.cpp
+++ src/zero_plus_minus_one.cpp
@@ -64,7 +64,7 @@ namespace tu {
    * @return true if and only if it is a 0-1 matrix
    */
 
-  bool is_zero_one_matrix (const integer_matrix& matrix, std::pair <size_t, size_t>& position)
+  bool is_zero_one_matrix (const integer_matrix& matrix, std::pair <integer_matrix::size_type, integer_matrix::size_type>& position)
   {
     for (size_t i = 0; i < matrix.size1(); ++i)
     {
