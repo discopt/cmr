b337dec0cd05972d22a7e3e3ca6cf1870b097a4a
Matthias Walter <xammy@xammy.homelinux.net>
2011-02-27 16:31:52 +0100

    Added things for Polymake test.

--- apps/common/src/unimodularity-test.cpp
+++ apps/common/src/unimodularity-test.cpp
@@ -1,4 +1,3 @@
-
 #include <Integer.h>
 #include <Matrix.h>
 #include <iostream>
@@ -6,53 +5,158 @@
 #include "total_unimodularity.hpp"
 #include "unimodularity.hpp"
 
-namespace polymake { namespace common {
+namespace polymake
+{
+  namespace common
+  {
 
-  bool is_totally_unimodular_plain(const Matrix<Integer>& matrix) {
-    unimod::integer_matrix input_matrix (matrix.rows(), matrix.cols());
-    for (size_t r = 0; r < input_matrix.size1(); ++r)
+    bool is_totally_unimodular_plain(const Matrix <Integer>& matrix)
     {
-      for (size_t c = 0; c < input_matrix.size2(); ++c)
+      unimod::integer_matrix input_matrix(matrix.rows(), matrix.cols());
+      for (size_t r = 0; r < input_matrix.size1(); ++r)
       {
-        input_matrix(r, c) = matrix(r, c);
+        for (size_t c = 0; c < input_matrix.size2(); ++c)
+        {
+          input_matrix(r, c) = matrix(r, c);
+        }
       }
+
+      return unimod::is_totally_unimodular(input_matrix);
     }
 
-    return unimod::is_totally_unimodular (input_matrix);
-  }
-  
-  bool is_totally_unimodular_violator(const Matrix<Integer>& matrix, Vector<Integer>& rows, Vector<Integer>& columns) {
-    unimod::integer_matrix input_matrix (matrix.rows(), matrix.cols());
-    for (size_t r = 0; r < input_matrix.size1(); ++r)
+    bool is_totally_unimodular_violator(const Matrix <Integer>& matrix, Vector <Integer>& rows, Vector <Integer>& columns)
     {
-      for (size_t c = 0; c < input_matrix.size2(); ++c)
-        input_matrix(r, c) = matrix(r, c);
+      unimod::integer_matrix input_matrix(matrix.rows(), matrix.cols());
+      for (size_t r = 0; r < input_matrix.size1(); ++r)
+      {
+        for (size_t c = 0; c < input_matrix.size2(); ++c)
+          input_matrix(r, c) = matrix(r, c);
+      }
+
+      unimod::submatrix_indices indices;
+
+      bool result = unimod::is_totally_unimodular(input_matrix, indices);
+      if (!result)
+      {
+        rows.resize(indices.rows.size());
+        for (size_t i = 0; i < indices.rows.size(); ++i)
+          rows[i] = (int) indices.rows[i];
+        columns.resize(indices.columns.size());
+        for (size_t i = 0; i < indices.columns.size(); ++i)
+          columns[i] = (int) indices.columns[i];
+      }
+      return result;
     }
 
-    unimod::submatrix_indices indices;
-
-    bool result = unimod::is_totally_unimodular (input_matrix, indices);
-    if (!result)
+    bool is_unimodular_plain(const Matrix <Integer>& matrix)
     {
-      rows.resize (indices.rows.size ());
-      for (size_t i = 0; i < indices.rows.size(); ++i)
-        rows[i] = (int) indices.rows[i];
-      columns.resize (indices.columns.size ());
-      for (size_t i = 0; i < indices.columns.size(); ++i)
-        columns[i] = (int) indices.columns[i];
+      size_t rank;
+      unimod::integer_matrix input_matrix(matrix.rows(), matrix.cols());
+      for (size_t r = 0; r < input_matrix.size1(); ++r)
+      {
+        for (size_t c = 0; c < input_matrix.size2(); ++c)
+        {
+          input_matrix(r, c) = matrix(r, c);
+        }
+      }
+
+      return unimod::is_unimodular(input_matrix, rank);
     }
-    return result;
-  }
+
+    bool is_strongly_unimodular_plain(const Matrix <Integer>& matrix)
+    {
+      size_t rank;
+      unimod::integer_matrix input_matrix(matrix.rows(), matrix.cols());
+      for (size_t r = 0; r < input_matrix.size1(); ++r)
+      {
+        for (size_t c = 0; c < input_matrix.size2(); ++c)
+        {
+          input_matrix(r, c) = matrix(r, c);
+        }
+      }
+
+      return unimod::is_strongly_unimodular(input_matrix, rank);
+    }
+
+    bool is_k_modular_plain(const Matrix <Integer>& matrix)
+    {
+      size_t rank;
+      unimod::integer_matrix input_matrix(matrix.rows(), matrix.cols());
+      for (size_t r = 0; r < input_matrix.size1(); ++r)
+      {
+        for (size_t c = 0; c < input_matrix.size2(); ++c)
+        {
+          input_matrix(r, c) = matrix(r, c);
+        }
+      }
+
+      return unimod::is_k_modular(input_matrix, rank);
+    }
+
+    bool is_k_modular_compute(const Matrix <Integer>& matrix, int& ko)
+    {
+      size_t rank;
+      unsigned int k;
+      bool result;
+      unimod::integer_matrix input_matrix(matrix.rows(), matrix.cols());
+      for (size_t r = 0; r < input_matrix.size1(); ++r)
+      {
+        for (size_t c = 0; c < input_matrix.size2(); ++c)
+        {
+          input_matrix(r, c) = matrix(r, c);
+        }
+      }
+
+      result = unimod::is_k_modular(input_matrix, rank, k);
+      ko = (int) k;
+      return result;
+    }
+
+    bool is_strongly_k_modular_plain(const Matrix <Integer>& matrix)
+    {
+      size_t rank;
+      unimod::integer_matrix input_matrix(matrix.rows(), matrix.cols());
+      for (size_t r = 0; r < input_matrix.size1(); ++r)
+      {
+        for (size_t c = 0; c < input_matrix.size2(); ++c)
+        {
+          input_matrix(r, c) = matrix(r, c);
+        }
+      }
+
+      return unimod::is_strongly_k_modular(input_matrix, rank);
+    }
 
   UserFunction4perl("# Tests a given //matrix// for total unimodularity, without certificates."
-        "# @param Matrix<int> matrix"
-        "# @return Bool",
-        &is_totally_unimodular_plain,"is_totally_unimodular(Matrix<Integer>)");
+      "# @param Matrix<int> matrix"
+      "# @return Bool",
+      &is_totally_unimodular_plain,"is_totally_unimodular(Matrix<Integer>)");
   UserFunction4perl("# Tests a given //matrix// for total unimodularity, setting row/column indices to a violating submatrix if the result is negative."
-        "# @param Matrix<int> matrix"
-        "# @param Vector<int> row_indices of violating submatrix."
-        "# @param Vector<int> column_indices of violating submatrix."
-        "# @return Bool",
-        &is_totally_unimodular_violator,"is_totally_unimodular(Matrix<Integer>,Vector<Integer>,Vector<Integer>)");
-} }
+      "# @param Matrix<int> matrix"
+      "# @param Vector<int> row_indices of violating submatrix."
+      "# @param Vector<int> column_indices of violating submatrix."
+      "# @return Bool",
+      &is_totally_unimodular_violator,"is_totally_unimodular(Matrix<Integer>,Vector<Integer>,Vector<Integer>)");
+  UserFunction4perl("# Tests a given //matrix// for unimodularity, without certificates."
+      "# @param Matrix<int> matrix"
+      "# @return Bool",
+      &is_unimodular_plain,"is_unimodular(Matrix<Integer>)");
+  UserFunction4perl("# Tests a given //matrix// for strong unimodularity, without certificates."
+      "# @param Matrix<int> matrix"
+      "# @return Bool",
+      &is_strongly_unimodular_plain,"is_strongly_unimodular(Matrix<Integer>)");
+  UserFunction4perl("# Tests a given //matrix// for k-modularity, without certificates."
+      "# @param Matrix<int> matrix"
+      "# @return Bool",
+      &is_k_modular_plain,"is_k_modular(Matrix<Integer>)");
+  UserFunction4perl("# Tests a given //matrix// for k-modularity, without certificates."
+      "# @param Matrix<int> matrix"
+      "# @param int& k"
+      "# @return Bool",
+      &is_k_modular_compute,"is_k_modular(Matrix<Integer>, $)");
+  UserFunction4perl("# Tests a given //matrix// for strong k-modularity, without certificates."
+      "# @param Matrix<int> matrix"
+      "# @return Bool",
+      &is_strongly_k_modular_plain,"is_strongly_k_modular(Matrix<Integer>)");
+}}
 
---
+++ test-nothing.pl
@@ -0,0 +1,54 @@
+# Call `polymake --script example.pl` after loading the extension, to run this example.
+
+use application "common";
+declare $matrix = new Matrix<Integer> (<<".");
+ 0 1 1 1 1 1
+ 1 1 0 0 1 1
+ 1 1 1 0 0 1
+ 1 0 1 1 0 1
+ 1 0 0 1 1 1
+.
+declare $rows = new Vector<Integer> ();
+declare $cols = new Vector<Integer> ();
+if (is_totally_unimodular($matrix, $rows, $cols))
+{
+  print "The matrix is totally unimodular.\n";
+}
+else
+{
+  print "The matrix is NOT totally unimodular.\n";
+  print "  Rows of the violating submatrix:    $rows\n";
+  print "  Columns of the violating submatrix: $cols\n";
+}
+if (is_unimodular($matrix))
+{
+  print "The matrix is unimodular.\n";
+}
+else
+{
+  print "The matrix is NOT unimodular.\n";
+}
+if (is_strongly_unimodular($matrix))
+{
+  print "The matrix is strongly unimodular.\n";
+}
+else
+{
+  print "The matrix is NOT strongly unimodular.\n";
+}
+if (is_k_modular($matrix))
+{
+  print "The matrix is k-modular.\n";
+}
+else
+{
+  print "The matrix is NOT k-modular.\n";
+}
+if (is_strongly_k_modular($matrix))
+{
+  print "The matrix is strongly k-modular.\n";
+}
+else
+{
+  print "The matrix is NOT strongly k-modular.\n";
+}
