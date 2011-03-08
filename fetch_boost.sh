70943be7bf933a0b8ef3b385e1f63e5a5fbb2a17
Matthias Walter <xammy@xammy.homelinux.net>
2011-03-08 12:15:50 +0100

    Added some missing boost dependencies. Setup the dist correctly.

--- fetch_boost.sh
+++ fetch_boost.sh
@@ -4,7 +4,7 @@ BOOST_URL=http://downloads.sourceforge.net/project/boost/boost/1.43.0/boost_1_43
 BOOST_TGZ=boost_1_43_0.tar.bz2
 
 BOOST_FOLDERS=
-for FOLDER in numeric/ublas serialization config mpl type_traits iterator tuple integer multi_index property_map concept smart_ptr optional archive dynamic_bitset parameter range bind python graph detail preprocessor utility exception unordered pending functional; do
+for FOLDER in numeric/ublas serialization config mpl type_traits iterator tuple integer multi_index property_map concept smart_ptr optional archive dynamic_bitset parameter range bind python graph detail preprocessor utility exception unordered pending functional random logic; do
   BOOST_FOLDERS="${BOOST_FOLDERS} boost_1_43_0/boost/${FOLDER}"
 done
 
