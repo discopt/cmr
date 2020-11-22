5b09d9209309c179ceaf5ac2e87677a11aa66ab9
Matthias Walter <matthias@matthiaswalter.org>
2020-11-22 23:54:35 +0100

    Added graphicness test stub.

---
+++ include/tu/graphic.h
@@ -0,0 +1,43 @@
+#ifndef TU_GRAPHIC_H
+#define TU_GRAPHIC_H
+
+#include <tu/env.h>
+#include <tu/matrix.h>
+#include <tu/graph.h>
+
+#include <stdio.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * \brief Tests a char matrix for graphicness.
+ *
+ * Returns \c true if and only if \p matrix is graphic.
+ *
+ * If \p pgraph is not \c NULL and if \p matrix is graphic, then \c *pgraph will point to a graph
+ * representing \p matrix, and it is set to \c NULL otherwise.
+ * If in addition to \p pgraph also \pbasis is not \c NULL, then this will be an array of edges
+ * forming the basis, i.e., indexing the rows of the matrix.
+ *
+ * If \p submatrix is not \c NULL and the matrix is not graphic, then a minimal nongraphic submatrix
+ * will be searched, which may cause extra computational effort. In this case, \c *submatrix will
+ * point to this submatrix for which the caller must use \ref TUsubmatrixFree to free memory.
+ * It is set to \c NULL otherwise.
+ */
+
+TU_EXPORT
+bool TUtestGraphicnessChr(
+  TU* tu,                 /**< TU environment */
+  TU_CHRMAT* matrix,      /**< Char matrix */
+  TU_LISTGRAPH** pgraph,  /**< If not \c NULL, the graph is stored. */
+  TU_LISTGRAPH_EDGE** pbasis, /**< If not \c NULL, contains (*pgraph)->numNodes-1 basis edges. */
+  TU_SUBMAT** submatrix   /**< If not \c NULL, a submatrix with bad determinant is stored. */
+);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* TU_GRAPHIC_H */
---
+++ src/tu/graphic.c
@@ -0,0 +1,15 @@
+// #define DEBUG_GRAPH
+
+#include <tu/graphic.h>
+
+#include "env_internal.h"
+
+#include <assert.h>
+
+bool TUtestGraphicnessChr(TU* tu, TU_CHRMAT* matrix, TU_LISTGRAPH** pgraph,
+  TU_LISTGRAPH_EDGE** pbasis, TU_SUBMAT** submatrix)
+{
+  
+
+  assert(false);
+}
