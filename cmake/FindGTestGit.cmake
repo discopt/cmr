f75d0c5e26d0d0232e0d28dc0d0a957f9a181839
Matthias Walter <matthias@matthiaswalter.org>
2021-11-26 17:20:19 +0100

    GoogleTest main branch was renamed from 'master' to 'main'.

--- cmake/FindGTestGit.cmake
+++ cmake/FindGTestGit.cmake
@@ -14,7 +14,7 @@ project(gtestgit-control NONE)
 include(ExternalProject)
 ExternalProject_Add(googletest
    GIT_REPOSITORY https://github.com/google/googletest.git
-   GIT_TAG master
+   GIT_TAG main
    SOURCE_DIR \"${CMAKE_BINARY_DIR}/gtestgit-src\"
    BINARY_DIR \"${CMAKE_BINARY_DIR}/gtestgit-build\"
    CONFIGURE_COMMAND \"\"
