403791171aa3c55a9a80d21b28dc6b6968273919
Matthias Walter <matthias@matthiaswalter.org>
2025-07-02 12:43:06 +0200

    Added debug warnings.

--- src/cmr/io_internal.h
+++ src/cmr/io_internal.h
@@ -10,6 +10,12 @@
 
 ssize_t getline(char** restrict lineptr, size_t* restrict n, FILE* restrict stream);
 
+#pragma message ( "_WIN32 is defined." )
+
+#else
+
+#pragma message ( "_WIN32 is NOT defined.")
+
 #endif /* _WIN32 */
 
 
